58c203f9fdd9aad5ac070d6537d4be916ca6b06d
Nipun Baryah <sunny.baryah.2602@gmail.com>
2024-11-29 00:04:17 +0530

    update editor component

--- src/components/common/Editor.tsx
+++ src/components/common/Editor.tsx
@@ -30,6 +30,11 @@ export default function MonacoEditor(props: EditorProps) {
   );
   const [value, setValue] = useState("");
   const [input, setInput] = useState<string>();
+  const [expectedOutputs, setExpectedOutputs] = useState<string[]>([]);
+  const [expectedOutput, setExpectedOutput] = useState<string>();
+  const [output, setOutput] = useState<string>();
+  const [outputs, setOutputs] = useState<string[]>([]);
+  const [err, setErr] = useState<string>("");
   const [selectedIndex, setSelectedIndex] = useState<number>(0);
   const [codeStatus, setCodeStatus] = useState<string>();
   const [loading, setLoading] = useState<boolean>(false);
@@ -54,6 +59,10 @@ export default function MonacoEditor(props: EditorProps) {
 
   useEffect(() => {
     setInput(props.test_cases[0].input);
+    const unhiddenCases=props.test_cases.filter((ele)=>ele.isHidden===false);
+    const exOutputs=unhiddenCases.map((ele)=>ele.expected_output);
+    setExpectedOutputs(exOutputs);
+    setExpectedOutput(exOutputs[0]);
   }, [props.test_cases]);
 
   const onMount: OnMount = (editor) => {
@@ -62,7 +71,9 @@ export default function MonacoEditor(props: EditorProps) {
   };
   const changeInput = (index: number) => {
     setSelectedIndex(index);
+    setExpectedOutput(expectedOutputs[index]);
     setInput(props.test_cases[index].input);
+    if (outputs.length > 0) setOutput(outputs[index]);
   };
 
   const getMostRepeatingElement = (arr: string[]): string => {
@@ -98,7 +109,27 @@ export default function MonacoEditor(props: EditorProps) {
           test_cases,
         });
 
-        // console.log("All statuses : ", response.data.data.allStatuses);
+        console.log("All statuses : ", response.data.data.allStatuses);
+
+        const stdouts: string[] = response.data.data.allStatuses.map(
+          (ele: { stdout: string }) => ele.stdout
+        ).filter((op:string)=>op!==null);
+        console.log("Outputs : ", stdouts);
+
+        const stderr: string[] = response.data.data.allStatuses.map(
+          (ele: { stderr: string }) => ele.stderr
+        );
+
+        const foundErrors = stderr.filter((op) => op !== null);
+        if (foundErrors.length > 0) {
+          setOutputs([]);
+          setErr(stderr[0]);
+        } else {
+          setOutputs(stdouts);
+          setOutput(stdouts[selectedIndex]);
+          setErr("");
+        }
+
         const results: string[] = response.data.data.allStatuses
           .reduce(
             (
@@ -364,33 +395,56 @@ export default function MonacoEditor(props: EditorProps) {
                   {codeStatus}
                 </h1>
               )}
-              <div className="flex gap-3 mb-5">
-                {props.test_cases &&
-                  props.test_cases.map((test_case, index) => {
-                    if (test_case.isHidden === false) {
-                      return (
-                        <button
-                          key={index}
-                          className={`py-1 px-2 rounded-md transition duration-200 ${
-                            selectedIndex === index
-                              ? "bg-gray-600 text-white"
-                              : "text-gray-300 hover:bg-gray-600 hover:text-white"
-                          }`}
-                          onClick={() => changeInput(index)}
-                        >
-                          {`Case ${index + 1} `}
-                        </button>
-                      );
-                    }
-                    return null;
-                  })}
-              </div>
-              <div className="flex gap-2 justify-center items-center">
-                <h3 className="font-semibold">Input : </h3>
-                <h3 className="text-gray-200 bg-gray-600 flex-grow pl-1 pr-2 py-2 rounded-md">
-                  {input}
-                </h3>
-              </div>
+              {err !== "" ? (
+                <div className="flex flex-col gap-2 justify-center items-start">
+                  <h1 className="text-xl font-semibold w-full">Error :</h1>
+                  <h2 className="w-full bg-red-700 rounded-lg px-3 py-2 text-red-300">{err}</h2>
+                </div>
+              ) : (
+                <div> 
+                  <div className="flex gap-3 mb-5">
+                    {props.test_cases &&
+                      props.test_cases.map((test_case, index) => {
+                        if (test_case.isHidden === false) {
+                          return (
+                            <button
+                              key={index}
+                              className={`py-1 px-2 rounded-md transition duration-200 ${
+                                selectedIndex === index
+                                  ? "bg-gray-600 text-white"
+                                  : "text-gray-300 hover:bg-gray-600 hover:text-white"
+                              }`}
+                              onClick={() => changeInput(index)}
+                            >
+                              {`Case ${index + 1} `}
+                            </button>
+                          );
+                        }
+                        return null;
+                      })}
+                  </div>
+                  <div className="flex flex-col gap-2 justify-center items-center">
+                    <h3 className="font-semibold w-full">Input : </h3>
+                    <h3 className="text-gray-200 bg-gray-600 flex-grow pl-1 pr-2 py-2 rounded-md w-full">
+                      {input}
+                    </h3>
+                  </div>
+                  <div className="mt-2 flex flex-col gap-2 justify-center items-center">
+                    <h3 className="font-semibold w-full">Expected Output : </h3>
+                    <h3 className="text-gray-200 bg-gray-600 flex-grow pl-1 pr-2 py-2 rounded-md w-full">
+                      {expectedOutput}
+                    </h3>
+                  </div>
+                </div>
+              )}
+              {outputs.length > 0 && (
+                <div className="mt-2 flex flex-col gap-2 justify-center items-center">
+                  <h3 className="font-semibold w-full">Output : </h3>
+                  <h3 className="text-gray-200 bg-gray-600 flex-grow pl-1 pr-2 py-2 rounded-md w-full">
+                    {output}
+                  </h3>
+                </div>
+              )}
             </motion.ul>
           )}
         </AnimatePresence>
